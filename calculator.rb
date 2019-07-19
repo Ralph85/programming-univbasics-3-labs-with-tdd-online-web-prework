0842a4ebe6130ac8d7aaca66ef8eb4a6b4bebd71
Ralph Saint Albord <ralph.saintalbord@yahoo.com>
2019-07-19 00:25:31 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,4 +1,6 @@
 first_number = 41.5
 second_number = 20
 sum = first_number + second_number
-difference = first_number + second_number
+difference = first_number - second_number
+product = first_number * second_number
+quotient = first_number / second_number
